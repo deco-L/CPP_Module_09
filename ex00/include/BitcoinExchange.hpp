0b6b2fd577f0cfcc87f70c0d40c4b27ecb0797b8
deco-L <scotchwitchbroom@gmail.com>
2024-05-27 16:27:20 +0900

    init

---
+++ ex00/include/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExcharnge.hpp                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
+/*   Updated: 2024/05/27 16:21:13 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <cstdlib>
+
+#endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,12 @@
 /*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 14:21:20 by csakamot          #+#    #+#             */
-/*   Updated: 2024/05/19 15:59:13 by csakamot         ###   ########.fr       */
+/*   Updated: 2024/05/27 16:23:04 by csakamot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(void) {
+  return (EXIT_SUCCESS);
+}
